d90a151d93b848a4ddffa4036dbf637055cd53a9
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2023-02-20 15:00:58 +0000

    Added date with JS

---
+++ script.js
@@ -0,0 +1,9 @@
+// let today = new Date();
+// let todayDisplay = document.querySelector('date');
+// todayDisplay = today;
+
+const date = new Date();
+document.getElementById("date").innerHTML = date.toLocaleString();
+
+
+
